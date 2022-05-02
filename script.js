39f0c561f2871aebc308c7b560f073515b665548
Pavan kalyan C <35896290+pavankalyan-codes@users.noreply.github.com>
2022-05-02 13:48:47 +0530

    reverted landing page changes back

--- script.js
+++
@@ -1,9 +0,0 @@
-let cardBgs=["radial-gradient(#1fe4f5, #3fbafe)","radial-gradient(#fbc1cc, #fa99b2)","radial-gradient(#76b2fe, #b69efe)","radial-gradient(#60efbc, #58d5c9)","radial-gradient(#f588d8, #c0a3e5)"]
-const cardSheet=document.getElementById('card-sheet');
-const openSheet=()=>{
-    console.log("foo");
-    cardSheet.style.height="100%"
-}
-const closeSheet=()=>{
-    cardSheet.style.height="0%"
-}
